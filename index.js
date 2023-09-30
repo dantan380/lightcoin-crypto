e72d1269202197b857e70f83868dea1cd03ec5a8
Daniel Tan <danieltan380@gmail.com>
2023-09-29 20:13:43 -0700

    refactored to use classes and inheritance

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
+    constructor(amount, account){
+        this.amount = amount;
+        this.account = account;
+    }
 
-  constructor(amount) {
-    this.amount = amount;
-  }
+    commit() {
+        if (!this.isAllowed()) return false;
+        this.time = new Date();
+        this.account.addTransaction(this);
+        return true;
+    }
+};
 
-  commit() {
-    balance -= this.amount;
-  }
+class Deposit extends Transaction {
+    get value() {
+        return this.amount;
+    }
 
+    isAllowed() {
+        return true;
+    }
+};
+
+class Withdrawal extends Transaction {
+    get value() {
+        return -this.amount;
+    }
+
+    isAllowed() {
+        return (this.account.balance - this.amount >= 0);
+    }
+};
+
+class Account {
+    constructor(username) {
+        this.username = username;
+        this.transactions = [];
+    }
+
+    get balance() {
+        for (let t of this.transactions) {
+            balance += t.value;
+        }
+        return balance;
+    }
+
+    addTransaction(transaction) {
+        this.transactions.push(transaction);
+    }
 }
-
-
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
+console.log('Starting Account balance: ', myAccount.balance);
+console.log('----');
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+t1 = new Deposit(120.00, myAccount);
+console.log('Commit Result', t1.commit());
+console.log('Deposited: ', t1.amount);
+console.log('Account balance: ', myAccount.balance);
+console.log('----');
 
-console.log('Balance:', balance);
+t2 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Withdrew: ', t2.amount);
+console.log('Account balance: ', myAccount.balance);
+console.log('----');
+
+
+t3 = new Deposit(1980, myAccount);
+console.log('Commit Result', t3.commit());
+console.log('Deposited: ', t3.amount);
+console.log('Account balance: ', myAccount.balance);
+console.log('----');
